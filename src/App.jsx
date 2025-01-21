3e251b222e2d932e8681f84d41befce3339d7cc6
Bikram Thapa <bikramt755@gmail>
2025-01-22 08:23:57 +1100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <div className="calculator" id="calculator">
+      <div className="display">
+        <div className="previous-operand"></div>
+        <div className="current-operand"></div>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
